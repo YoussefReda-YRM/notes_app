fc413030dc8bde5f73e4017e29bf9c8d5f3bfce0
youssef <yrda3414@gmail.com>
2023-01-19 20:19:26 +0200

    create bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -8,11 +8,29 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed:(){},
+        onPressed:()
+        {
+          showModalBottomSheet(
+            context: context,
+            builder: (context)
+            {
+              return const AddNoteBottomSheet();
+            },
+          );
+        },
         child: const Icon(Icons.add),
       ),
       body: const NotesViewBody(),
     );
+  }
+}
+
+class AddNoteBottomSheet extends StatelessWidget {
+  const AddNoteBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
