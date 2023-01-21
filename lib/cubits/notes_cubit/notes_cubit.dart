b50630a737f0f05e2a9e86723078f4734584b9d0
youssef <yrda3414@gmail.com>
2023-01-22 01:45:36 +0200

    display notes 1

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -10,17 +10,10 @@ class NotesCubit extends Cubit<NotesState>
 {
   NotesCubit() : super(NotesInitial());
 
-  fetchAllNotes()async
+  List<NoteModel>? notes;
+  fetchAllNotes()
   {
-    try
-    {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    }
-    catch(e)
-    {
-      emit(NotesFailure(e.toString()));
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -4,17 +4,3 @@ part of 'notes_cubit.dart';
 abstract class NotesState {}
 
 class NotesInitial extends NotesState{}
-class NotesLoading extends NotesState{}
-class NotesSuccess extends NotesState
-{
-  final List<NoteModel> note;
-
-  NotesSuccess(this.note);
-}
-
-class NotesFailure extends NotesState
-{
-  final String errorMessage;
-  
-  NotesFailure(this.errorMessage);
-}
